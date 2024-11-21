75d811565de8f61d356e695256de4b305ca60c19
jayodele64 <jayodele64@gmail.com>
2024-11-21 12:49:29 +0100

    remove comment in styles

--- src/lib/Modal/index.tsx
+++ src/lib/Modal/index.tsx
@@ -9,9 +9,8 @@ interface ModalProps {
 }
 
 const Modal = ({ isOpen, onClose = () => null, children }: ModalProps) => {
-  if (!isOpen) return null; // If the modal isn't open, don't render anything
+  if (!isOpen) return null;
 
-  // Render modal using React Portal
   return ReactDOM.createPortal(
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
@@ -21,7 +20,7 @@ const Modal = ({ isOpen, onClose = () => null, children }: ModalProps) => {
         {children}
       </div>
     </div>,
-    document.body, // Render the modal at the body element
+    document.body,
   );
 };
 
